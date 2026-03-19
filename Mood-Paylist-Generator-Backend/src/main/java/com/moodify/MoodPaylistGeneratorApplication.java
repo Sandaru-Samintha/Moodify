283dd41065d6ba9b2c6d00812a96806b2173a73f
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-03-19 20:18:00 +0530

    Upload the front end and backend

---
+++ Mood-Paylist-Generator-Backend/src/main/java/com/moodify/MoodPaylistGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.moodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodPaylistGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodPaylistGeneratorApplication.class, args);
+	}
+
+}
