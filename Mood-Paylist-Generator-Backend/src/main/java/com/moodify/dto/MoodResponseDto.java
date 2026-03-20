4501184522bb90735e3576acbd3e33b82673d368
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-03-20 19:55:06 +0530

    create the mood response dto

---
+++ Mood-Paylist-Generator-Backend/src/main/java/com/moodify/dto/MoodResponseDto.java
@@ -0,0 +1,21 @@
+package com.moodify.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoodResponseDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String color;
+    private String icon;
+    private Float energy;
+    private Float danceability;
+    private Float valence;
+    private Integer tempo;
+}
