a07320f29334545806a90d6ecc90fc5f0d5d1bbf
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-03-20 19:21:56 +0530

    create the loging request dto

---
+++ Mood-Paylist-Generator-Backend/src/main/java/com/moodify/dto/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package com.moodify.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+    @NotBlank(message = "Username or email is required")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
