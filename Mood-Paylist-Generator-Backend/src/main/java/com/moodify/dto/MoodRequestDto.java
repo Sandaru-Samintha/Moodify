29d4a5eec5efee01ac12d3e5ec3122f161796985
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-03-20 19:31:49 +0530

    create the mood request dto

---
+++ Mood-Paylist-Generator-Backend/src/main/java/com/moodify/dto/MoodRequestDto.java
@@ -0,0 +1,25 @@
+package com.moodify.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoodRequestDto {
+    @NotBlank(message = "Mood name is required")
+    private String name;
+
+    private String description;
+    private String color;
+    private  String icon;
+
+    private  Float energy;
+    private Float danceability;
+    private Float valence;
+    private Integer tempo;
+}
