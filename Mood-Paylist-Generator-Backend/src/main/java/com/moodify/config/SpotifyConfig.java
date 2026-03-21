2ec65956a5aaf6255c6d3eadb0a6b1e53d3e02ab
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2026-03-21 12:55:53 +0530

    Create the security config and spotify config

---
+++ Mood-Paylist-Generator-Backend/src/main/java/com/moodify/config/SpotifyConfig.java
@@ -0,0 +1,21 @@
+package com.moodify.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class SpotifyConfig {
+
+    @Value("${spotify.client.id}")
+    private String clientId;
+
+    @Value("${spotify.client.secret}")
+    private String clientSecret;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
